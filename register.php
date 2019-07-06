6b4fa9044da6248dd468ff50f24ebc0217a9d600
prabhdeepsingh007 <43317101+prabhdeepsingh007@users.noreply.github.com>
2019-07-06 18:39:49 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome User</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
+</head>
+<body>
+<br>	
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					
+				</div>
+				<div class="col-sm-6">
+					<form method="post">
+						<div class="row">
+			        		<h2 style="text-align: center; color: #ffffff; background-color: #332f2f;    box-shadow: 1px 0px 3px 0px;">Welcome User</h2>
+			        	</div><br>
+						<div class="row">
+							<div class="col-sm-6">
+								<label>Name</label><br>
+								<input class="form-control" type="text" name="fname" placeholder="Enter First Name" required>
+							</div>
+							
+						</div>
+
+						<br>
+
+						<div class="row">
+							<div class="col-sm-6">
+								<label>Email</label><br>
+								<input class="form-control" type="text" name="email" placeholder="Enter Email" required>
+							</div>
+							
+							<div class="col-sm-6">
+								<label>Phone Number</label><br>
+								<input class="form-control" type="text" name="pno" placeholder="Enter Email" required>
+							</div>
+							
+						</div>
+<br>
+						
+						<div class="row">
+							<div class="col-sm-6">
+								<label>Representing any company?</label><br>
+								<input class="form-control" type="text" name="comp" placeholder="Enter Email" required>
+							</div>
+
+							<div class="col-sm-6">
+								<label>Company/Organisation Name</label><br>
+								<input class="form-control" type="text" name="comp_name" placeholder="Enter Email" required>
+							</div>
+							
+						</div>
+<br>
+						<div class="row">
+							<div class="col-sm-6">
+								<label style="font-size: 20px;"><u>Details of the Project</u></label><br>
+							</div>
+							
+						</div>
+
+						<br>
+
+						<div class="row">
+							<div class="col-sm-6">
+								<label>How many playgrounds do you want to build?</label><br>
+								<input class="form-control" type="text" name="play" placeholder="Enter Email" required>
+							</div>
+						</div>
+<br>
+						<div class="row">
+							<div class="col-sm-6">
+								<label>Do you have a location in mind?</label><br>
+								<input class="form-control" type="text" name="loc" placeholder="Enter Email" required>
+							</div>
+						</div>
+<br>
+						<div class="row">
+							<div class="col-sm-6">
+								<label>Preferred Budget</label><br>
+								<input class="form-control" type="text" name="budget" placeholder="Enter Email" required>
+							</div>
+						</div>
+<br>
+						<div class="row">
+							<div class="col-sm-3">
+								
+							</div>
+
+							<div class="col-sm-6">
+								<input class="form-control" type="button" name="Submit" value="Submit" placeholder="Enter Email" required>
+							</div>
+						</div>
+
+					</form>
+
+					<br>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
